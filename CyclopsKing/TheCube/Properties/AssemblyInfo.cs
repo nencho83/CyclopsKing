c51593ed4b90d0713d7f7f413a8d4bb1b66ff1b7
nencho83 <nencho83@yahoo.com>
2015-03-04 22:42:36 +0200

    AsseblyInfo modified

--- CyclopsKing/TheCube/Properties/AssemblyInfo.cs
+++ CyclopsKing/TheCube/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TheCube")]
+[assembly: AssemblyTitle("ConsoleApplication1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TheCube")]
+[assembly: AssemblyProduct("ConsoleApplication1")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("5a720276-d08c-46a6-851c-24772004bc01")]
+[assembly: Guid("7a05a429-2698-4fa6-bcf2-c4356c6e944e")]
 
 // Version information for an assembly consists of the following four values:
 //
